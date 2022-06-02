c96dacb374d4bbe79af36a961d8eaa10389647a7
Owen Delahoy <owen.delahoy@kidsloop.live>
2022-06-02 19:08:43 +0900

    build: add release configration

---
+++ libs/config/release.config.js
@@ -0,0 +1,36 @@
+
+   
+const name = 'config';
+const srcRoot = `libs/${name}`;
+
+module.exports = {
+  extends: 'release.config.base.js',
+  pkgRoot: `dist/${srcRoot}`,
+  tagFormat: name + "-v${version}",
+  commitPaths: [`${srcRoot}/*`],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: `${srcRoot}/CHANGELOG.md`,
+      },
+    ],
+    [
+        '@semantic-release/npm',
+        {
+            npmPublish: true,
+        },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: [`${srcRoot}/package.json`, `${srcRoot}/CHANGELOG.md`],
+        message:
+          `release(version): Release ${name} ` +
+          '${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
